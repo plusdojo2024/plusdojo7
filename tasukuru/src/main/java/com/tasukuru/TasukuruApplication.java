f4d4f471a9d118f4f4347c1234bb19a79a8dfce6
plusdojo2024-matsui <matsui-ryunosuke-plusdojo2024@seplus2016.onmicrosoft.com>
2024-07-18 15:36:02 +0900

    test

---
+++ tasukuru/src/main/java/com/tasukuru/TasukuruApplication.java
@@ -0,0 +1,13 @@
+package com.tasukuru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasukuruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasukuruApplication.class, args);
+	}
+
+}
---
+++ tasukuru/src/test/java/com/tasukuru/TasukuruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasukuru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasukuruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
