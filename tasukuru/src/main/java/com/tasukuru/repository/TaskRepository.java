fa35212bfd621ccaa49be0db007269c685b73056
plusdojo2024-kajihara <kajiwara-rai-plusdojo2024@seplus2016.onmicrosoft.com>
2024-07-18 16:17:03 +0900

    ファイル作成

---
+++ tasukuru/src/main/java/com/tasukuru/repository/TaskRepository.java
@@ -0,0 +1,7 @@
+//package com.tasukuru.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface TaskRepository extends JpaRepository<T, ID> {
+//
+//}
