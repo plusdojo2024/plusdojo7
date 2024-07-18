632a4e5880201a99cb0978a47355b240c6a3da57
plusdojo2024-endo <endo-madoka-plusdojo2024@seplus2016.onmicrosoft.com>
2024-07-18 16:43:59 +0900

    お小遣い帳Repository

---
+++ tasukuru/src/main/java/com/tasukuru/repository/KidesUserRepository.java
@@ -0,0 +1,7 @@
+//package com.tasukuru.repository;
+
+//import org.springframework.data.jpa.repository.JpaRepository;
+
+//public interface KidesUserRepository extends JpaRepository<KidsUsers, Integer> {
+
+//}
---
+++ tasukuru/src/main/java/com/tasukuru/repository/SupportRepository.java
@@ -0,0 +1,7 @@
+//package com.tasukuru.repository;
+
+//import org.springframework.data.jpa.repository.JpaRepository;
+
+//public interface SupportRepository extends JpaRepository<Suport, Integer> {
+
+//}
