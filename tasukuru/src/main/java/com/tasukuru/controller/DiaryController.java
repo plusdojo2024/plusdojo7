acbe3cb0bf3971aaa97a7ec57e32e8186dcb071f
plusdojo2024-asai <asai-kazuki-plusdojo2024@seplus2016.onmicrosoft.com>
2024-07-18 16:33:59 +0900

    update task

---
+++ tasukuru/src/main/java/com/tasukuru/controller/DiaryController.java
@@ -0,0 +1,13 @@
+package com.tasukuru.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class DiaryController {
+	@GetMapping
+	public String index(Model model) {
+		model.addAttribute("message", "こんにちは！");
+		return "index";
+	}
+}
---
+++ tasukuru/src/main/java/com/tasukuru/controller/KidTaskController.java
@@ -0,0 +1,14 @@
+package com.tasukuru.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class KidTaskController {
+	@GetMapping
+	public String index(Model model) {
+		model.addAttribute("message", "こんにちは！");
+		return "index";
+	}
+}
