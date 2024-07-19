a00e68cbf1d03c39ce8b9c6a68c6577b8e415c34
plusdojo2024-inagawa <inagawa-takumi-plusdojo2024@seplus2016.onmicrosoft.com>
2024-07-19 11:12:39 +0900

    エネミーEntity

--- tasukuru/src/main/java/com/tasukuru/entity/Enemie.java
+++ tasukuru/src/main/java/com/tasukuru/entity/Enemie.java
@@ -1,5 +1,27 @@
 package com.tasukuru.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name="enemies")
+
 public class Enemie {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private Integer hp;
+	private Integer drop;
+	private String name;
+	private String image;
 }
