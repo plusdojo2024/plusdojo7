4c7578d3305a6849dcad3ab713b24064074ef49b
plusdojo2024-togawa <togawa-takumi-plusdojo2024@seplus2016.onmicrosoft.com>
2024-07-19 13:54:21 +0900

    フォルダ削除

--- react-tasukuru/src/Money/MoneyChild.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import axios from "axios";
-
-export default class MoneyChild extends React.Component{
-    render(){
-        return <div>MoneyChild</div>
-    };
-}
--- react-tasukuru/src/Money/MoneyParent.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import axios from "axios";
-
-export default class MoneyParent extends React.Component{
-    render(){
-        return <div>MoneyParent</div>
-    };
-}
