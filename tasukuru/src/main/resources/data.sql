367e264a5159b03baaca0c1cfe0ea36ec6427640
plusdojo2024-asai <asai-kazuki-plusdojo2024@seplus2016.onmicrosoft.com>
2024-07-19 14:47:00 +0900

    Update data.sql

--- tasukuru/src/main/resources/data.sql
+++ tasukuru/src/main/resources/data.sql
@@ -1,1 +1,8 @@
-tesr
+INSERT INTO tasks (kids_id, name, content, regtime, categories_name, tasklimit, submittime, taskimage, comment, review_one, review_two, review_three, taskcheck, no_complete, complete, miss) VALUES 
+(101, '宿題', '算数の宿題をする', '2024-07-19T09:00:00', '勉強', '2024-07-20T18:00:00', NULL, 'homework.png', '頑張ってね！', TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE);
+
+INSERT INTO tasks (kids_id, name, content, regtime, categories_name, tasklimit, submittime, taskimage, comment, review_one, review_two, review_three, taskcheck, no_complete, complete, miss) VALUES 
+(102, '掃除', '部屋を掃除する', '2024-07-19T10:00:00', '家事', '2024-07-20T12:00:00', NULL, 'cleaning.png', '綺麗にしてね！', FALSE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE);
+
+INSERT INTO tasks (kids_id, name, content, regtime, categories_name, tasklimit, submittime, taskimage, comment, review_one, review_two, review_three, taskcheck, no_complete, complete, miss) VALUES 
+(103, '読書', '本を30分読む', '2024-07-19T11:00:00', '趣味', '2024-07-21T18:00:00', NULL, 'reading.png', '楽しんでね！', FALSE, FALSE, TRUE, FALSE, FALSE, FALSE, FALSE);
