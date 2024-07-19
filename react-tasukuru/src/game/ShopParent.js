906cee556b0ab3e19168ab3f647758a28a81d594
plusdojo2024-Awano <awano-masato-plusdojo2024@seplus2016.onmicrosoft.com>
2024-07-19 15:44:27 +0900

    ShopParent

--- react-tasukuru/src/game/ShopParent.js
+++ react-tasukuru/src/game/ShopParent.js
@@ -1,18 +1,51 @@
 import React from "react";
 import axios from "axios";
+import './ParentShop.css';
+
+export default class ShopParent extends React.Component{
+    
+    constructor(props) {
+        super(props);
+        //stateの設定。
+        this.state = {
+            shops:[],
+            id:"",
+            kid_id:"",
+            name:"",
+            price:"",
+            condition:"",
+            showModal:false
+        }
+        this.state={
+            requests:[],
+            id:"",
+            kid_id:"",
+            name:"",
+            showModal:false
+        }
 
 
-export default class Game extends React.Component{
-    
-    
-    
-    
-    
-    
-    
-    
-    
+        
+    }
+    onChange = (e) => {
+        this.setState({
+            keyword: e.target.value
+        });
+    }
+
+
+
     render(){
-        return <div>ShopParent</div>
-    };
-}
+         const{title} = this.state;
+        return(
+        <div class="shopName">
+            <h1>保護者用ショップ設定</h1>
+        </div>
+        );
+        }
+
+
+
+
+
+   }
