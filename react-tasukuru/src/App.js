7c0f58bb2376046b24acb2afd288c27619101f3a
plusdojo2024-kajihara <kajiwara-rai-plusdojo2024@seplus2016.onmicrosoft.com>
2024-07-19 10:47:26 +0900

    diarie関連

--- react-tasukuru/src/App.js
+++ react-tasukuru/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
-import Login from './login/Login';
+import Login from './login/login';
 import Task from './tasks/Task';
+
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
