7806bdab0555c7aa95bc5d9b4267ecd57de66e37
plusdojo2024-theingi <theingi-htwe-plusdojo2024@seplus2016.onmicrosoft.com>
2024-07-19 15:44:07 +0900

    Diary

--- tasukuru/src/main/java/com/tasukuru/controller/DiaryController.java
+++ tasukuru/src/main/java/com/tasukuru/controller/DiaryController.java
@@ -1,13 +1,22 @@
 package com.tasukuru.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.tasukuru.entity.Diary;
+import com.tasukuru.repository.DiaryRepository;
 @Controller
 public class DiaryController {
+	
+	@Autowired
+	private DiaryRepository repository;
+	
 	@GetMapping("/diary")
 	public String index(Model model) {
-		model.addAttribute("message", "こんにちは！");
-		return "index";
+		Iterable<Diary> diary = repository.findAll();
+		model.addAttribute("diary",diary);
+		return "diary";
 	}
 }
