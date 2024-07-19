fef66f41bdb05fa84b06af5231f73777e6c5d62f
plusdojo2024-asai <asai-kazuki-plusdojo2024@seplus2016.onmicrosoft.com>
2024-07-19 10:47:44 +0900

    Update Task.java

--- tasukuru/src/main/java/com/tasukuru/entity/Task.java
+++ tasukuru/src/main/java/com/tasukuru/entity/Task.java
@@ -1,5 +1,40 @@
 package com.tasukuru.entity;
 
-public class Task {
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "tasks")
+public class Task {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private Integer kids_id;
+	private String name;
+	private String content;
+	private LocalDateTime regtime;
+	private String categories;
+	private LocalDateTime limit;
+	private LocalDateTime submitTime;
+	private String taskImage;
+	private String comment;
+	private Boolean review_one;
+	private Boolean review_two;
+	private Boolean review_three;
+	private Boolean check;
+	private Boolean no_complete;
+	private Boolean complete;
+	private Boolean miss;
 }
