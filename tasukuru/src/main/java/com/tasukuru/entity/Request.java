2a0e2716af0ae177058c4d2fcecc3c77140f92c3
plusdojo2024-Awano <awano-masato-plusdojo2024@seplus2016.onmicrosoft.com>
2024-07-19 10:49:26 +0900

    requests,shops関連

--- tasukuru/src/main/java/com/tasukuru/entity/Request.java
+++ tasukuru/src/main/java/com/tasukuru/entity/Request.java
@@ -1,5 +1,25 @@
 package com.tasukuru.entity;
 
-public class Request {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name="requests")
+public class Request {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private Integer kid_id;
+	private String name;
+	
 }
