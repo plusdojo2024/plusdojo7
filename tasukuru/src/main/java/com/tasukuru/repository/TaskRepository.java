feca8dba5f0c640c987df1a092429447a03c8aa6
plusdojo2024-asai <asai-kazuki-plusdojo2024@seplus2016.onmicrosoft.com>
2024-07-19 15:32:55 +0900

    Update TaskRepository.java

--- tasukuru/src/main/java/com/tasukuru/repository/TaskRepository.java
+++ tasukuru/src/main/java/com/tasukuru/repository/TaskRepository.java
@@ -1,10 +1,13 @@
 package com.tasukuru.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.tasukuru.entity.Task;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Integer> {
-	
+	//レコードを登録日時とタスク期限で並び替え
+	Iterable<Task> findAllByOrderByRegtimeAscTasklimitAsc();
 }
 
