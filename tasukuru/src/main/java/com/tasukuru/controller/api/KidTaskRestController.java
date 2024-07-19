d94e3c6739edc08c1be7e0fcf84f972d12e5e0f7
plusdojo2024-asai <asai-kazuki-plusdojo2024@seplus2016.onmicrosoft.com>
2024-07-19 16:32:53 +0900

    Create KidTaskRestController.java

---
+++ tasukuru/src/main/java/com/tasukuru/controller/api/KidTaskRestController.java
@@ -0,0 +1,26 @@
+package com.tasukuru.controller.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tasukuru.entity.Task;
+import com.tasukuru.repository.TaskRepository;
+
+@RestController
+public class KidTaskRestController {
+	@Autowired
+	private TaskRepository repository;
+	
+	@GetMapping("/api/task/")
+	private Iterable<Task> get(){
+		return repository.findAll();
+	}
+	
+	@GetMapping("/api/task/add/")
+	private Task addTask(@RequestBody Task task){
+		repository.save(task);
+		return task;
+	}
+}
