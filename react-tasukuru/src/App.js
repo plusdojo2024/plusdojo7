d1b847635d81092f1ef852aea199d7f1f538017d
plusdojo2024-theingi <theingi-htwe-plusdojo2024@seplus2016.onmicrosoft.com>
2024-07-19 10:03:14 +0900

    ルーティング（URLによるページ分け）Task Login ページ

--- react-tasukuru/src/App.js
+++ react-tasukuru/src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './login/Login';
+import Task from './tasks/Task';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login></Login>}></Route>
+            <Route path="/task" element={<Task></Task>}></Route>
+          </Routes>
+        </BrowserRouter>
+        
       </header>
     </div>
   );
