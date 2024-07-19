d2922754fabeed36d39c2559877ee160ff272c72
plusdojo2024-matsui <matsui-ryunosuke-plusdojo2024@seplus2016.onmicrosoft.com>
2024-07-19 16:19:00 +0900

    更新

--- react-tasukuru/src/login/Login.js
+++ react-tasukuru/src/login/Login.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Header from '../foundation/Header.js';
+import Footer from '../foundation/Footer.js';
 
 export default class Login extends React.Component{
     render(){
         return(
-          <div>Login111</div>
-        ) 
+          <div>
+            
+            Login111
+            <img src="./images/tasukuru.png" className="App-logo" alt="logo" />
+          </div>
+        )
     };
 }
