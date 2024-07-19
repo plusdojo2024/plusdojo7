45bcd45011c2a50a49214222ab652d229da69684
plusdojo2024-matsui <matsui-ryunosuke-plusdojo2024@seplus2016.onmicrosoft.com>
2024-07-19 13:15:38 +0900

    更新

--- react-tasukuru/src/login/login.js
+++ react-tasukuru/src/login/login.js
@@ -2,6 +2,7 @@ import React from "react";
 
 export default class Login extends React.Component{
     render(){
-        return <div>Login</div>
+        return 
+        <div>Login11</div>
     };
 }
