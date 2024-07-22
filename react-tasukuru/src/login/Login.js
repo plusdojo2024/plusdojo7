18afe240c33f6b5975a1bd496699b70ee1e08d28
plusdojo2024-matsui <matsui-ryunosuke-plusdojo2024@seplus2016.onmicrosoft.com>
2024-07-22 17:23:02 +0900

    更新

--- react-tasukuru/src/login/Login.js
+++ react-tasukuru/src/login/Login.js
@@ -1,25 +1,36 @@
 import React from "react";
+import { useNavigate } from "react-router-dom"; // useNavigate をインポートする
 import LoginHeader from '../foundation/LoginHeader.js';
 import LoginFooter from "../foundation/LoginFooter.js";
 import './Login.css';
 
-export default class Login extends React.Component {
-    render() {
-        return (
-            <div>
-                <div className="login_header">
-                    <LoginHeader />
-                </div>
-                <div className="background_image_renga">
-                    <body className="login_body">
-                        <button className="loginF_button">保護者</button><br />
-                        <button className="loginK_button">こども</button>
-                    </body>
-                </div>
-                <div className="login_footer">
-                    <LoginFooter />
+export default function Login() {
+    const navigate = useNavigate(); // useNavigate フックを使ってナビゲーションオブジェクトを取得する
+
+    const FamilyClick = () => {
+        // ボタンがクリックされたら /loginFamily に遷移する
+        navigate('/loginFamily');
+    };
+
+    const KidsClick = () => {
+        // ボタンがクリックされたら /loginFamily に遷移する
+        navigate('/loginKids');
+    };
+
+    return (
+        <div>
+            <div className="login_header">
+                <LoginHeader />
+            </div>
+            <div className="background_image_renga">
+                <div className="login_body">
+                    <button className="loginF_button" onClick={FamilyClick}>保護者</button><br />
+                    <button className="loginK_button" onClick={KidsClick}>こども</button><br />
                 </div>
             </div>
-        );
-    }
+            <div className="login_footer">
+                <LoginFooter />
+            </div>
+        </div>
+    );
 }
--- react-tasukuru/src/start/Start.js
+++ react-tasukuru/src/start/Start.js
@@ -3,6 +3,7 @@ import './Start.css';
 
 export default class Start extends React.Component{
     render(){
+        
         return(
             <div className="Start">
               <img src="./images/tasukuru.png" className="start" alt="start" />
