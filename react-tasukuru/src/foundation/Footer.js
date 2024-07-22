fcc6cdb70fb3bfddb86f4ebab94d4aad9417e38d
plusdojo2024-yamamoto-ayato <yamamoto-ayato-plusdojo2024@seplus2016.onmicrosoft.com>
2024-07-22 15:18:58 +0900

    ヘッダー、フッター

--- react-tasukuru/src/foundation/Footer.js
+++ react-tasukuru/src/foundation/Footer.js
@@ -5,10 +5,10 @@ export default class Footer extends React.Component{
         return(
             <div className="footer">
                 <div className="footer_allicons">
-                <img className="footer_icon1"src=""></img>
-                <img className="footer_icon2"src=""></img>
-                <img className="footer_icon3"src=""></img>
-            </div>
+                    <img className="footer_icon"src="images/1415.png" alt="タスク"></img>
+                    <img className="footer_icon"src="images/849.png" alt="おこづかい"></img>
+                    <img className="footer_icon"src="images/ic060.png" alt="ゲーム"></img>
+                </div>
             </div>
         )
     }
