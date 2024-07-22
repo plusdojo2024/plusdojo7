2bfd9ee9286a2615a51c1e18e62832e85059f5e1
plusdojo2024-inagawa <inagawa-takumi-plusdojo2024@seplus2016.onmicrosoft.com>
2024-07-22 15:29:51 +0900

    ゲーム画面

--- react-tasukuru/src/game/Game.js
+++ react-tasukuru/src/game/Game.js
@@ -1,7 +1,44 @@
 import React from "react";
+import './Game.css';
+import Header from './GameHeader.js';
+import Footer from '../foundation/Footer.js';
+import './GameBattle.js';
+import GameBattle from "./GameBattle.js";
+import GameDaice from "./GameDice.js";
 
 export default class Game extends React.Component{
+    //親コンポーネントから受け取るデータなどがpropsに入っている。
+    constructor(props) {
+        super(props);
+        //stateの設定。
+        this.state = {
+            enemies:[] ,
+            id:"",
+            hp:"",
+            drop:"",
+            name: "",
+            image: "",
+            }
+
+    } 
+    
+    
     render(){
-        return <div>Game</div>
-    };
+        //画面項目に連動するstateはここで宣言する。
+        // const { id, hp, drop, name, image } = this.state;
+        return (    
+                
+            <div id="body">
+                <Header />
+
+                <main>
+                    <GameBattle />
+                    <GameDaice />
+                </main>
+
+                <Footer />
+            </div>
+            
+    );
+}
 }
