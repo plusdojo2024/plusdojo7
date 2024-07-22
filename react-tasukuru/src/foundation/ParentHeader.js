50f0ce71e65517e78e68b5e0b7accd5882e88138
plusdojo2024-yamamoto-ayato <yamamoto-ayato-plusdojo2024@seplus2016.onmicrosoft.com>
2024-07-22 17:47:24 +0900

    ヘッダーフッター

---
+++ react-tasukuru/src/foundation/ParentHeader.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+export default class ParentHeader extends React.Component{
+    render(){
+        return(
+            <div className="header_parent">
+                {/* ログアウトボタン */}
+                <button className="header_logout" onClick={this.logout} >ログアウト</button>
+                {/* 個人名 */}
+                <div className="header_name">ひろし</div>
+                
+
+                <hr></hr>
+            </div>
+                    );
+                }
+            }
