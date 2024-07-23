01166a20e9a270489e229a3d89271e5aeea600ae
plusdojo2024-asai <asai-kazuki-plusdojo2024@seplus2016.onmicrosoft.com>
2024-07-23 10:20:01 +0900

    Update Task.js

--- react-tasukuru/src/tasks/Task.js
+++ react-tasukuru/src/tasks/Task.js
@@ -2,17 +2,32 @@ import React from "react";
 import Header from '../foundation/Header';
 import Footer from '../foundation/Footer';
 import './Task.css';
+import axios from "axios";
 
-
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, json } from 'react-router-dom';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 export default class Task extends React.Component{
-    
+  constructor(props){
+    super(props);
+    this.state = {
+      tasks:[],
+    }
+  }
+
+    componentDidMount(){
+        //タスクデータの取得
+        fetch("/api/task/")
+        .then(res => res.json())
+        .then(json => {
+            console.log(json);
+            this.setState({tasks: json});
+        });
+    }
     
     render(){
-        
+        const { tasks } = this.state;
+
         return (
             
         <wrapper>
@@ -22,21 +37,24 @@ export default class Task extends React.Component{
                     <div className="background">
                     <Tabs>
     <TabList className="TabList">
-      <Tab className="Tab_title">未たっせい</Tab>
-      <Tab className="Tab_title">かんりょう</Tab>
-      <Tab className="Tab_title">しっぱい</Tab>
+      <Tab>未たっせい</Tab>
+      <Tab>かんりょう</Tab>
+      <Tab>しっぱい</Tab>
     </TabList>
-    <div className="Tab_content">
+
     <TabPanel>
-      <h2>Any content 1</h2>
+      <div className="button_container">
+        <button className="diary_button">日記登録</button>
+        <button className="task_add_button">追加</button>
+      </div>
     </TabPanel>
+
     <TabPanel>
       <h2>Any content 2</h2>
     </TabPanel>
     <TabPanel>
       <h2>Any content 3</h2>
     </TabPanel>
-    </div>
   </Tabs>
                     </div>
                 </div>
