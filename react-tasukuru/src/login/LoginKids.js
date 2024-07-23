2308ee4348f34f64324b106569b128ded3669ff2
plusdojo2024-matsui <matsui-ryunosuke-plusdojo2024@seplus2016.onmicrosoft.com>
2024-07-23 11:30:43 +0900

    kousinn

--- react-tasukuru/src/login/LoginKids.js
+++ react-tasukuru/src/login/LoginKids.js
@@ -1,26 +1,33 @@
 import React from "react";
+import { useNavigate } from "react-router-dom"; // useNavigate をインポートする
 import LoginHeader from '../foundation/LoginHeader.js';
 import LoginFooter from "../foundation/LoginFooter.js";
 import './LoginKids.css';
 
-export default class LoginKids extends React.Component {
-    render() {
-        return (
-            <div>
-                <div className="login_header">
-                    <LoginHeader />
-                </div>
-                <div className="background_image_renga">
-                    <div className="loginKids_body">
-                        <input type="text" placeholder="家族ID" className="textbox" /><br />
-                        <input type="text" placeholder="なまえ" className="textbox" /><br />
-                        <button className="loginMain_button">ろぐいん</button><br />
-                    </div>
-                </div>
-                <div className="login_footer">
-                    <LoginFooter />
+export default function LoginKids() {
+    const navigate = useNavigate(); // useNavigate フックを使ってナビゲーションオブジェクトを取得する
+    
+    const ReturnClick = () => {
+        // ボタンがクリックされたら /login に遷移する
+        navigate('/login');
+    };
+
+    return (
+        <div>
+            <div className="login_header">
+                <LoginHeader />
+            </div>
+            <div className="background_image_renga">
+                <div className="loginKids_body">
+                    <input type="text" placeholder="家族ID" className="textbox" /><br />
+                    <input type="text" placeholder="なまえ" className="textbox" /><br />
+                    <button className="loginMain_button">ろぐいん</button><br />
+                    <button className="loginR_button" onClick={ReturnClick}>←</button><br />
                 </div>
             </div>
-        );
-    }
+            <div className="login_footer">
+                <LoginFooter />
+            </div>
+        </div>
+    );
 }
--- react-tasukuru/src/start/Start.js
+++ react-tasukuru/src/start/Start.js
@@ -1,13 +1,18 @@
 import React from "react";
 import './Start.css';
+import { useNavigate } from "react-router-dom"; // useNavigate をインポートする
 
-export default class Start extends React.Component{
-    render(){
-        
-        return(
-            <div className="Start">
-              <img src="./images/tasukuru.png" className="start" alt="start" />
-            </div>
-        );
-    }
+export default function LoginKids() {
+    const navigate = useNavigate(); // useNavigate フックを使ってナビゲーションオブジェクトを取得する
+    
+    const ReturnClick = () => {
+        // ボタンがクリックされたら /login に遷移する
+        navigate('/login');
+    };
+
+    return (
+        <div className="Start" onClick={ReturnClick}>
+            <img src="./images/tasukuru.png" className="start" alt="start" />
+        </div>
+    );
 }
