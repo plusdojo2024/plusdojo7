4dc3d64fb915da2ba1a0e535afb420c25b7a2dde
plusdojo2024-matsui <matsui-ryunosuke-plusdojo2024@seplus2016.onmicrosoft.com>
2024-07-24 14:32:20 +0900

    更新

--- tasukuru/src/main/java/com/tasukuru/repository/FamilyUserRepository.java
+++ tasukuru/src/main/java/com/tasukuru/repository/FamilyUserRepository.java
@@ -1,9 +1,14 @@
 package com.tasukuru.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.tasukuru.entity.FamilyUser;
 
+@Repository
 public interface FamilyUserRepository extends JpaRepository<FamilyUser, String> {
+
+    // 家族IDとパスワードを基にユーザーを検索するメソッド
+	FamilyUser findByFamilyIdAndPass(String familyID, String password);
 
 }
