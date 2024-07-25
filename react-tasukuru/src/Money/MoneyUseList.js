d5d40b2cd9b9e511e037b6b80569f3b7c67ec595
plusdojo2024-endo <endo-madoka-plusdojo2024@seplus2016.onmicrosoft.com>
2024-07-25 11:56:36 +0900

    お小遣い一覧

--- react-tasukuru/src/Money/MoneyUseList.js
+++ react-tasukuru/src/Money/MoneyUseList.js
@@ -15,7 +15,7 @@ class MoneyUseList　extends React.Component {
      //データ取得
     componentDidMount(){
         //学習用にaxiosでなく、標準のfetchを利用している。
-        fetch("/api/money/current")
+        fetch("/api/money/list")
         //取得したレスポンスを JSON 形式に変換
         .then(res => res.json())
         //JSON データを取得した後、コンソールにログ出力
@@ -30,22 +30,51 @@ class MoneyUseList　extends React.Component {
         });
     }
 
+    toggleModal = () => {
+        const{showModal} = this.state;
+        this.setState({
+            showModal: !showModal
+        });
+    }
+
+    onInput = (e) => {
+        //コントロールの名前を取得する
+        const name= e.target.name;
+        //コントロールに入力した値をstateに更新する。
+        this.setState({
+            [name]: e.target.value
+        });
+    }
+
+    modMoney = () => {
+        //利用するstateの値を宣言
+        const { allowances } = this.state;
+
+        //stateの値を利用してpostデータを作成
+        const data = {
+            date:allowances,
+            item: "",
+            amount: "",
+        };
+
+        this.toggleModal();
+    }
+
     render() {
-        const { usageHistory } = this.props;
+        const { allowances } = this.state;
         return (
             <div>
                 <h2>使ったお金一覧</h2>
-                {usageHistory.map((monthData, index) => (
-                    <div key={index}>
-                        <h3>{monthData.month}</h3>
-                        <ul>
-                            {monthData.items.map((item, idx) => (
-                                <li key={idx}>
-                                    使用金額: {item.amount} 円 - 用途: {item.description}
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
+                {allowances.map((allowance, index) => (
+                   <div class="use">
+                   <p>{allowance.moneyDate}&nbsp;&nbsp;{allowance.usedType}&nbsp;&nbsp;{allowance.usedMoney}</p>
+                   
+                   {/* Todo:編集ボタンを押すとmodBook関数が実行されるようにする。引数はindex */}
+                   <button onClick={() => {this.modBook(index)}}>編集</button>
+                  
+                   </div>
+             
+
                 ))}
             </div>
         );
