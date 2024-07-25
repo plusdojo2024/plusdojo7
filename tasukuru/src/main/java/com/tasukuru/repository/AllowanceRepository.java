a351d072352d8ef89ae20e813edb18116e88f778
plusdojo2024-togawa <togawa-takumi-plusdojo2024@seplus2016.onmicrosoft.com>
2024-07-25 09:36:54 +0900

    お小遣いrepository

--- tasukuru/src/main/java/com/tasukuru/repository/AllowanceRepository.java
+++ tasukuru/src/main/java/com/tasukuru/repository/AllowanceRepository.java
@@ -6,4 +6,6 @@ import com.tasukuru.entity.Allowance;
 
 public interface AllowanceRepository extends JpaRepository<Allowance, Integer> {
 
+	//IDを指定し、AllowanceEntityの情報を取得する
+		public Allowance findById(int Id);
 }
