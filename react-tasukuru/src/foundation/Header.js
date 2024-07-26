c988c0fa9bdef2b503100751f3c7a6ecd4ea3d2e
plusdojo2024-masubuchi <masubuchi-souma-plusdojo2024@seplus2016.onmicrosoft.com>
2024-07-26 13:42:21 +0900

    更新

--- react-tasukuru/src/foundation/Header.js
+++ react-tasukuru/src/foundation/Header.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import './Header.css';
 export default class Header extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            CharacterSkin :"",
+        }
+    }
+
+    componentDidMount() {
+            
+        // 子供ユーザーデータを取得
+        fetch("/api/mypage/")
+        .then(res => res.json())
+        .then(CharacterSkin => {
+            console.log(CharacterSkin); // 子供ユーザーデータをコンソールに出力（確認用）
+            this.setState({ CharacterSkin });
+        })
+        .catch(error => console.error("エラー", error));
+        
+    }
+        
     render(){
+        const {CharacterSkin}=this.state;
         return(
 
            <div className="header">
@@ -11,7 +32,9 @@ export default class Header extends React.Component{
                 <div className="header_name">ひろし</div>
                 {/* アイコン */}
                 <div className="header_icon">
-                    <img src="../images/character_yusha_01_red.png" alt="アバター"></img>
+                    {CharacterSkin&&(
+                    <img src={CharacterSkin.characterImage} alt="アバター"></img>
+                    )}
                 </div>
                 <hr></hr>
             </div> 
