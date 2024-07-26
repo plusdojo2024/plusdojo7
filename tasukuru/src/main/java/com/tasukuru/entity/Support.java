f21b3646fcf4dc8d57bcbaa806f6e2c4af2e8518
plusdojo2024-togawa <togawa-takumi-plusdojo2024@seplus2016.onmicrosoft.com>
2024-07-26 10:59:44 +0900

    お小遣いdate.sql

--- tasukuru/src/main/java/com/tasukuru/entity/Support.java
+++ tasukuru/src/main/java/com/tasukuru/entity/Support.java
@@ -1,8 +1,6 @@
 package com.tasukuru.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -19,7 +17,7 @@ public class Support {
 
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	
 	private Integer id;
 	private Boolean condition;
 	private Integer damage;
